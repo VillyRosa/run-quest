1f55121c57d3ffc977843217772338a9eea7b660
VillyRosa <villyrosa@hotmail.com>
2025-04-11 19:04:20 +0000

    feat(race): integrate timer from RunService in race page

--- src/app/pages/race/race.page.ts
+++ src/app/pages/race/race.page.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonButton } from '@ionic/angular/standalone';
+import { RunService } from 'src/app/services/run.service';
 
 @Component({
   selector: 'app-race',
@@ -11,4 +12,14 @@ import { IonContent, IonButton } from '@ionic/angular/standalone';
   imports: [IonButton, IonContent, CommonModule, FormsModule]
 })
 export class RacePage {
+  private runService = inject(RunService);
+  public timer$ = this.runService.getTime();
+
+  public ionViewDidEnter(): void {
+    this.runService.start();
+  }
+
+  public onFinish(): void {
+    this.runService.stop();
+  }
 }
