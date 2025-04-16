ec404744644ff976a3de32fc928d9c5ef6254b9a
VillyRosa <villyrosa@hotmail.com>
2025-04-16 14:14:13 +0000

    feat(auth-start): add translation

--- src/app/pages/auth-start/auth-start.page.ts
+++ src/app/pages/auth-start/auth-start.page.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonButton } from '@ionic/angular/standalone';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-auth-start',
   templateUrl: './auth-start.page.html',
   styleUrls: ['./auth-start.page.scss'],
   standalone: true,
-  imports: [IonButton, IonContent, CommonModule, FormsModule, RouterModule]
+  imports: [IonButton, IonContent, CommonModule, FormsModule, RouterModule, TranslateModule]
 })
 export class AuthStartPage {
 }
