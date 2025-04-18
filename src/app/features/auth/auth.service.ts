f09f6e55e992d1f3b8cb70e4339339ca08b7121d
Villy Rosa <villyrosa@hotmail.com>
2025-04-17 23:23:16 -0300

    feat(auth): integrate login page with backend authentication

--- src/app/features/auth/auth.service.ts
+++ src/app/features/auth/auth.service.ts
@@ -2,19 +2,33 @@ import { inject, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastService } from '../../shared/services/toast.service';
 import { TranslateService } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { LoadingController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private http = inject(HttpClient);
   private router = inject(Router);
+  private loadingCtrl = inject(LoadingController)
   private toastService = inject(ToastService);
   private translate = inject(TranslateService)
 
-  public login(email: string, password: string): void {
-    localStorage.setItem('token', 'token');
-    this.router.navigate(['/']);
-    this.toastService.success(this.translate.instant('WELCOME') + '!');
+  public async login(email: string, password: string): Promise<void> {
+    const loading = await this.loadingCtrl.create();
+    await loading.present();
+
+    this.http.post<string>('auth/login', { email, password}, { responseType: 'text' as 'json' }).subscribe({
+      next: (response: string) => {
+        localStorage.setItem('token', response);
+        this.router.navigate(['/']);
+        this.toastService.success(this.translate.instant('WELCOME') + '!');
+      },
+      error: () => {
+        this.toastService.error('Erro ao tentar autenticar. Tente novamente.');
+      }
+    }).add(() => loading.dismiss());
   }
 
   public logout(): void {
--- src/app/features/auth/login/login.page.ts
+++ src/app/features/auth/login/login.page.ts
@@ -1,20 +1,41 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IonContent, IonButton, IonInput, IonItem, IonInputPasswordToggle } from '@ionic/angular/standalone';
 import { AuthService } from 'src/app/features/auth/auth.service';
+import { ToastService } from 'src/app/shared/services/toast.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonItem, IonInput, IonButton, IonContent, CommonModule, FormsModule, IonInputPasswordToggle]
+  imports: [IonItem, IonInput, IonButton, IonContent, CommonModule, FormsModule, IonInputPasswordToggle, ReactiveFormsModule]
 })
 export class LoginPage {
   private authService = inject(AuthService);
+  private fb = inject(FormBuilder);
+  private toastService = inject(ToastService);
+
+  public form: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  });
 
   public onSubmit(): void {
-    this.authService.login('email', 'password');
+    if (this.form.valid) {
+      const { email, password } = this.form.value;
+      this.authService.login(email, password);
+    } else {
+      if (this.form.get('email')?.hasError('required')) {
+        this.toastService.error('E-mail é obrigatório.');
+      } else if (this.form.get('email')?.hasError('email')) {
+        this.toastService.error('Formato de e-mail inválido.');
+      } else if (this.form.get('password')?.hasError('required')) {
+        this.toastService.error('Senha é obrigatória.');
+      } else {
+        this.toastService.error('Por favor, preencha todos os campos corretamente.');
+      }
+    }
   }
 }
