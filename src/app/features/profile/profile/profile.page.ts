b9fda90551de006637288bbd22e1bc3663b8e6ff
VillyRosa <villyrosa@hotmail.com>
2025-04-24 20:43:33 +0000

    feat(profile): add translation

--- src/app/features/profile/profile/profile.page.ts
+++ src/app/features/profile/profile/profile.page.ts
@@ -4,13 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { IonContent, IonButton } from '@ionic/angular/standalone';
 import { AuthService } from 'src/app/features/auth/auth.service';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
   standalone: true,
-  imports: [IonButton, IonContent, CommonModule, FormsModule, RouterModule]
+  imports: [IonButton, IonContent, CommonModule, FormsModule, RouterModule, TranslateModule]
 })
 export class ProfilePage {
   private authService = inject(AuthService);
