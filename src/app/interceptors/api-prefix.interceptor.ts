5f7bf0e0d415b09e2d39eee3868ae0ee3fdf4d99
Villy Rosa <villyrosa@hotmail.com>
2025-04-24 13:19:22 -0300

    feat(http): add auth token to API requests in interceptor

--- src/app/interceptors/api-prefix.interceptor.ts
+++ src/app/interceptors/api-prefix.interceptor.ts
@@ -1,12 +1,16 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { TokenService } from '../shared/services/token.service';
 
 export const apiPrefixInterceptor: HttpInterceptorFn = (req, next) => {
   const isFullUrl = req.url.startsWith('http');
   const isAsset = req.url.includes('assets');
 
+  const isAuthenticated = TokenService.hasToken();
+
   const apiReq = (isFullUrl || isAsset) ? req : req.clone({
-    url: `${environment.api.replace(/\/$/, '')}/${req.url.replace(/^\//, '')}`
+    url: `${environment.api.replace(/\/$/, '')}/${req.url.replace(/^\//, '')}`,
+    setHeaders: isAuthenticated ? { Authorization: `Bearer ${TokenService.getToken()}` } : {}
   });
 
   return next(apiReq);
