4abca058f3c1b117995fe9c3b892d8cdb1441a40
VillyRosa <villyrosa@hotmail.com>
2025-04-24 18:51:05 +0000

    feat(profile): add translation to run history page

--- src/app/features/profile/run-history/run-history.page.ts
+++ src/app/features/profile/run-history/run-history.page.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonList, IonRippleEffect } from '@ionic/angular/standalone';
 import { RaceService } from 'src/app/features/race/race.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-run-history',
   templateUrl: './run-history.page.html',
   styleUrls: ['./run-history.page.scss'],
   standalone: true,
-  imports: [IonRippleEffect, IonList, IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonContent, CommonModule, FormsModule]
+  imports: [IonRippleEffect, IonList, IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonContent, CommonModule, FormsModule, TranslateModule]
 })
 export class RunHistoryPage {
   private raceService = inject(RaceService);
