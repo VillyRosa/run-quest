10e27f4d56ce91d837470a6d8a02d2b5b62184ed
VillyRosa <villyrosa@hotmail.com>
2025-04-30 14:51:15 +0000

    feat(me-service): add get, update and change password methods

--- src/app/shared/services/me.service.ts
+++ src/app/shared/services/me.service.ts
@@ -1,12 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { User } from '../models/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MeService {
   private http = inject(HttpClient);
+
+  public getMyAccount(): Observable<User> {
+    return this.http.get<User>('/users/me');
+  }
+
+  public updateMyAccount(user: User): Observable<User> {
+    return this.http.put<User>('/users/me', user);
+  }
+
+  public updateMyPassword(password: string, confirmPassword: string): Observable<void> {
+    return this.http.put<void>('/users/me/password', { password, confirmPassword });
+  }
 
   public deleteMyAccount(): Observable<void> {
     return this.http.delete<void>('/users/me');
