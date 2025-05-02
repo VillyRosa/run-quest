05d0249005aa67591990cbe1fb4578ad3b025f34
VillyRosa <villyrosa@hotmail.com>
2025-05-02 13:51:39 +0000

    feat(race-result): show real values after race

--- src/app/features/race/race-result/race-result.page.ts
+++ src/app/features/race/race-result/race-result.page.ts
@@ -1,22 +1,22 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
 import { ActivatedRoute, RouterModule } from '@angular/router';
+import { RaceService } from '../race.service';
+import { SecondsToTimePipe } from 'src/app/shared/pipes/seconds-to-time.pipe';
 
 @Component({
   selector: 'app-race-result',
   templateUrl: './race-result.page.html',
   styleUrls: ['./race-result.page.scss'],
   standalone: true,
-  imports: [IonCol, IonRow, IonGrid, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, RouterModule]
+  imports: [IonCol, IonRow, IonGrid, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, RouterModule, SecondsToTimePipe]
 })
-export class RaceResultPage implements OnInit {
+export class RaceResultPage {
   private route = inject(ActivatedRoute);
+  private raceService = inject(RaceService);
 
-  public runId: string | null = null;
-
-  public ngOnInit(): void {
-    this.runId = this.route.snapshot.paramMap.get('id');
-  }
+  public runId: string = this.route.snapshot.paramMap.get('id')!;
+  public run$ = this.raceService.getById(this.runId);
 }
