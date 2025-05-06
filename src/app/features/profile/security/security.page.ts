bf6085e412c4117622e22add95fa3be7847d09fc
VillyRosa <villyrosa@hotmail.com>
2025-05-06 20:45:21 +0000

    refactor(security): move delete my account for security page

--- src/app/features/profile/security/security.page.ts
+++ src/app/features/profile/security/security.page.ts
@@ -1,19 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonContent, IonHeader, IonToolbar, IonTitle, IonList, IonItem, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonToolbar, IonTitle, IonList, IonItem, IonIcon, IonLabel, IonAlert } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { chevronForward, lockClosedOutline } from 'ionicons/icons';
+import { chevronForward, lockClosedOutline, alertCircleOutline } from 'ionicons/icons';
 import { RouterModule } from '@angular/router';
+import { MeService } from 'src/app/shared/services/me.service';
+import { AuthService } from '../../auth/auth.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-security',
   templateUrl: './security.page.html',
   styleUrls: ['./security.page.scss'],
   standalone: true,
-  imports: [IonLabel, IonIcon, IonItem, IonList, IonTitle, IonToolbar, IonHeader, IonContent, CommonModule, RouterModule]
+  imports: [IonAlert, IonLabel, IonIcon, IonItem, IonList, IonTitle, IonToolbar, IonHeader, IonContent, CommonModule, RouterModule, TranslateModule]
 })
 export class SecurityPage {
+  private meService = inject(MeService);
+  private authService = inject(AuthService);
+
+  public deleteAccountAlertButtons = [
+    {
+      text: 'Cancelar',
+      role: 'cancel'
+    },
+    {
+      text: 'Deletar',
+      role: 'confirm',
+      handler: () => this.deleteAccount(),
+    },
+  ];
+
   constructor() {
-    addIcons({ chevronForward, lockClosedOutline });
+    addIcons({ chevronForward, lockClosedOutline, alertCircleOutline });
+  }
+
+  public deleteAccount(): void {
+    this.meService.deleteMyAccount().subscribe({
+      next: () => {
+        this.authService.logout();
+      }
+    });
   }
 }
