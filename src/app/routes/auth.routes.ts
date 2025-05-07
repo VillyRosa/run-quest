f3c9321a9ca446b31e992384dc7c1ac9b22ddb95
VillyRosa <villyrosa@hotmail.com>
2025-05-07 13:28:46 +0000

    chore: add @pages alias to tsconfig and update related imports

--- src/app/routes/auth.routes.ts
+++ src/app/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from "@angular/router";
-import { AuthStartPage } from "../pages/auth-start/auth-start.page";
+import { AuthStartPage } from "@pages/auth-start/auth-start.page";
 import { LoginPage } from "@features/auth/login/login.page";
 import { RegisterPage } from "@features/auth/register/register.page";
 import { ForgotPasswordPage } from "@features/auth/forgot-password/forgot-password.page";
--- src/app/routes/home.routes.ts
+++ src/app/routes/home.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from "@angular/router";
-import { HomePage } from "../pages/home/home.page";
+import { HomePage } from "@pages/home/home.page";
 import { RacePage } from "@features/race/race/race.page";
 import { RaceResultPage } from "@features/race/race-result/race-result.page";
 
