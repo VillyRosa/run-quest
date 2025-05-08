a0c14ce1d77e40aad9aa9ef5c492ef005ee8e534
VillyRosa <villyrosa@hotmail.com>
2025-05-08 11:14:54 +0000

    chore(app): update app name and app ID

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'run-quest',
+  appId: 'run.runquest.app',
+  appName: 'Run Quest',
   webDir: 'www'
 };
 
