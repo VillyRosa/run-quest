ff5f4d46d5828ca4848e59237eb8ad45872a7cca
VillyRosa <villyrosa@hotmail.com>
2025-05-08 11:45:53 +0000

    feat(profile): add alert event to confirm logout

--- src/app/features/profile/profile/profile.page.ts
+++ src/app/features/profile/profile/profile.page.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonButton, IonAlert } from '@ionic/angular/standalone';
 import { AuthService } from '@features/auth/auth.service';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
@@ -11,10 +11,22 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
   standalone: true,
-  imports: [IonButton, IonContent, CommonModule, FormsModule, RouterModule, TranslateModule]
+  imports: [IonAlert, IonButton, IonContent, CommonModule, FormsModule, RouterModule, TranslateModule]
 })
 export class ProfilePage {
   private authService = inject(AuthService);
+
+  public logoutAlertButtons = [
+    {
+      text: 'Cancelar',
+      role: 'cancel'
+    },
+    {
+      text: 'Sair',
+      role: 'confirm',
+      handler: () => this.onLogout(),
+    },
+  ];
 
   public onLogout(): void {
     this.authService.logout();
